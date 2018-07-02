2e495b2d8b2b5191122927a97343946fe8fb6998
hong <hong_ej@yeah.net>
2018-07-02 13:31:48 +0800

    入门桶排序

--- cn.lhr.sort/demo1.java
+++ cn.lhr.sort/demo1.java
@@ -1,5 +1,27 @@
 /**
+ * 入门桶排序
+ * 时间复杂度为
  * Created by LHR on 2018/7/2.
  */
+//   53528 从大到小排序， 排序后是 8 5 5 3 2
 public class demo1 {
+    public static void main(String[] args) {
+        int[] a = new int[11];
+        int[] b = {5,3,5,2,8};
+
+        //给数字每个数赋值为0
+        for (int i = 0; i < a.length; i++) {
+            a[i] = 0;
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            a[b[i]] ++ ;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int i1 = 0; i1 < a[i]; i1++) {
+                System.out.println(i);
+            }
+        }
+    }
 }
